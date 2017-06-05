54cd971aedbb4823ce4d03da6ef5b99d5e52c14c
ORB-Union <take5079@gmail.com>
2017-06-05 23:02:23 +0900

    장애물 오브젝트(손,부글부글, 도끼) 추가 및 폴더 정리 및 스프라이트 애니메이션 수정

--- Kuriko-master/Assets/kuriko.anim/기본이동/스크립트/PlayerControl.cs
+++ Kuriko-master/Assets/kuriko.anim/기본이동/스크립트/PlayerControl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayerControl : MonoBehaviour {
 
     private AudioSource audio_jump; // 오디오소스에 점프소리를 읽음
@@ -24,7 +24,7 @@ public class PlayerControl : MonoBehaviour {
     float directionX = 0;
     bool walking = false;
     public float Speed = 8.0f; // 걷는 속도 8
-    public float J_P = 15.0f; // 점프력   15
+    public float J_P = 10.0f; // 점프력   10
     bool jumping = false;
 
     public float health;
@@ -52,6 +52,27 @@ public class PlayerControl : MonoBehaviour {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();  // rigidbody에서 지원하는 AddForce를 사용하기 위해
         
+    }
+    void Jump()
+    {
+        // 죽었을때 or 'Active'라는 애니메이션이 애니메이터에서 작동될때 동작정지
+        if (death == true || animator.GetCurrentAnimatorStateInfo(0).nameHash == Animator.StringToHash("Base Layer.Active"))
+            return;
+        else // 아닐 때
+        {
+            if (jumping == false && Input.GetKeyDown(KeyCode.Space)) // 스페이스를 Down(누르면)하고 Jumping == false일 떄만 점프동작
+            {
+                jumping = true;
+                rb.AddForce(Vector3.up * J_P, ForceMode.VelocityChange); // 위쪽방향으로 J_P값만큼 점프한다.
+                this.audio_jump.clip = this.Jumping; // 오디오클립이 점프하는사운드를 읽음
+                this.audio_jump.loop = false; // 반복재생 안함
+                this.audio_jump.Play(); // 점프할 때 만 사운드를 재생
+            }
+            if (rb.velocity.y == 0)
+            {
+                jumping = false;
+            }
+        }
     }
     void Move()
     {
@@ -81,11 +102,6 @@ public class PlayerControl : MonoBehaviour {
                     facingright = false;
 
                 }
-                //else if(v>0)
-                //{
-                //    directionX = 0;
-                //}
-
                 else
                 {
                     walking = false;
@@ -109,27 +125,13 @@ public class PlayerControl : MonoBehaviour {
         if(health <= 0)
         {
             Death();
+
         }
         Move();
-       
-             if (jumping == false && Input.GetKeyDown(KeyCode.Space) ) // 스페이스를 Down(누르면)하고 Jumping == false일 떄만 점프동작
-             {
-                jumping = true;
-                rb.AddForce(Vector3.up * J_P, ForceMode.VelocityChange); // 위쪽방향으로 J_P값만큼 점프한다.
-                this.audio_jump.clip = this.Jumping; // 오디오클립이 점프하는사운드를 읽음
-                this.audio_jump.loop = false; // 반복재생 안함
-                this.audio_jump.Play(); // 점프할 때 만 사운드를 재생
-            }
-             
-            if(rb.velocity.y == 0)
-            {
-                jumping = false;
-            }
-
+        Jump();
         //Z키 누를시 박스생성 및 생성 지연시간 추가
 
         if (Input.GetKeyDown(KeyCode.Z))
-
         {
             health -= 2.0f;
             Invoke("CreateBoxFire", 1.2f); // 박스 생성지연 , 시간(1.0f초)
@@ -137,7 +139,6 @@ public class PlayerControl : MonoBehaviour {
             //BoxPosition.transform.position = CreateBoxPosition.transform.position; 
             //CreateBoxFire(); // 아래에 있음
         }
-
         UpdateHealthbar();
     }
 
@@ -155,11 +156,26 @@ public class PlayerControl : MonoBehaviour {
     //진입상태
     void OnTriggerStay(Collider other)
     {
-        if (other.tag == "Handi")
+        if (other.tag == "Axe")
+        {
+            health -= 0.2f;
+            Death();
+        }
+
+        
+        if (other.tag == "BoggleBoggle")
+        {
+            health -= 0.15f;
+            Death();
+        }
+        
+
+        if (other.tag == "Hand")
         {
             health -= 0.1f;
             Death();
         }
+
         UpdateHealthbar();
     }
 
@@ -176,7 +192,7 @@ public class PlayerControl : MonoBehaviour {
             if (!facingright)
             {
                 //방향조정
-                transform.localScale = new Vector3(0.6f * directionX, 0.5f, 1.0f);
+                transform.localScale = new Vector3(0.6f * directionX, 0.4f, 1.0f);
                 //생성위치 조정
                 Instantiate(leftCreateBoxPosition, Createpos.position, Quaternion.identity);  // 캐릭터앞에 ray 쏴서 박스 놓을 공간 확인
             }
@@ -185,14 +201,14 @@ public class PlayerControl : MonoBehaviour {
             if (facingright)
             {
                 //방향 조정
-                transform.localScale = new Vector3(0.6f * directionX, 0.5f, 1.0f);
+                transform.localScale = new Vector3(0.6f * directionX, 0.4f, 1.0f);
                 //생성 위치 조정
                 Instantiate(rightCreateBoxPosition, Createpos.position * directionX, Quaternion.identity);
 
 
             }
             //위치 재조정
-            transform.localScale = new Vector3(0.5f, 0.5f, 1.0f);
+            transform.localScale = new Vector3(0.4f, 0.4f, 2.0f);
         }
     }
 
@@ -204,8 +220,16 @@ public class PlayerControl : MonoBehaviour {
         {
             death = true;
             animator.SetBool("Dies", true);
+
+			Invoke("Gogameover",1);
+
         }
     }
+
+	void Gogameover()
+	{
+		SceneManager.LoadScene ("Gameover");
+	}
 
     void UpdateHealthbar()
     {
--- Kuriko-master/Assets/kuriko.anim/오브젝트/스크립트/SujungBreak.cs
+++ Kuriko-master/Assets/kuriko.anim/오브젝트/스크립트/SujungBreak.cs
@@ -66,7 +66,7 @@ public class SujungBreak : MonoBehaviour {
         if (other.tag == "Kuriko" && Heading == true)
         {
             animator.SetBool("Breaking", true);
-            DestroyObject(gameObject, 1.5f);
+            DestroyObject(gameObject, 1.2f);
         }
 
     }
