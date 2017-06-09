52fcefab7bb4d66fd6184ec8853b6582f2544613
ORB-Union <take5079@gmail.com>
2017-06-09 13:02:59 +0900

    스테이지1 완성 및 수정

--- Kuriko-master/Assets/kuriko.anim/기본이동/스크립트/PlayerControl.cs
+++ Kuriko-master/Assets/kuriko.anim/기본이동/스크립트/PlayerControl.cs
@@ -6,12 +6,17 @@ using UnityEngine.SceneManagement;
 public class PlayerControl : MonoBehaviour {
 
     private AudioSource audio_jump; // 오디오소스에 점프소리를 읽음
-    public AudioClip Jumping; //오디오 클립이 점프소리를 읽음
+    public AudioClip audio_Jumping; //오디오 클립이 점프소리를 읽음
+    private AudioSource audio_Attack;
+    public AudioClip audio_Attacking;
+    private AudioSource audio_Create;
+    public AudioClip audio_Creating;
 
     public Image currentHealthbar;
     public Text ratioText;
 
 	public Canvas Menu;//인게임메뉴캔버스용
+	public Canvas SoundMenu;//사운드메뉴캔버스용
 
     Animator animator;
     Rigidbody rb;
@@ -34,15 +39,19 @@ public class PlayerControl : MonoBehaviour {
     private float hitpoint;
     private float maxHitpoint;
     */
+    bool hit;
     bool death;
     bool facingright; // 위치 인식
 
     void Start()
     {
         this.audio_jump = this.gameObject.AddComponent<AudioSource>();
+        this.audio_Attack = this.gameObject.AddComponent<AudioSource>();
+        this.audio_Create = this.gameObject.AddComponent<AudioSource>();
         health = 100.0f; // 체력
         maxhealth = 100.0f;
 
+        hit = false;
         death = false;
         facingright = true;
         Createpos = transform.Find("CreateBoxPosition"); //생성위치 인식하기위해서(좌우 이동시)
@@ -56,6 +65,7 @@ public class PlayerControl : MonoBehaviour {
     }
     void Jump()
     {
+
         // 죽었을때 or 'Active, Heading'라는 애니메이션이 애니메이터에서 작동될때 동작정지 (스킬 사용 동안 점프불가)
         if (death == true || animator.GetCurrentAnimatorStateInfo(0).nameHash == Animator.StringToHash("Base Layer.Active")
             || animator.GetCurrentAnimatorStateInfo(0).nameHash == Animator.StringToHash("Base Layer.Heading"))
@@ -66,7 +76,7 @@ public class PlayerControl : MonoBehaviour {
             {
                 jumping = true;
                 rb.AddForce(Vector3.up * J_P, ForceMode.VelocityChange); // 위쪽방향으로 J_P값만큼 점프한다.
-                this.audio_jump.clip = this.Jumping; // 오디오클립이 점프하는사운드를 읽음
+                this.audio_jump.clip = this.audio_Jumping; // 오디오클립이 점프하는사운드를 읽음
                 this.audio_jump.loop = false; // 반복재생 안함
                 this.audio_jump.Play(); // 점프할 때 만 사운드를 재생
             }
@@ -78,13 +88,14 @@ public class PlayerControl : MonoBehaviour {
     }
     void Move()
     {
-        // 죽었을때 or 'Active, Heading'라는 애니메이션이 애니메이터에서 작동될때 동작정지 (스킬 사용 동안 걷기불가)
+        if (transform.position.x >= 28.3f && transform.position.y >= 3.6f)
+            return;
+            // 죽었을때 or 'Active, Heading'라는 애니메이션이 애니메이터에서 작동될때 동작정지 (스킬 사용 동안 걷기불가)
         if (death == true || animator.GetCurrentAnimatorStateInfo(0).nameHash == Animator.StringToHash("Base Layer.Active")
-            || animator.GetCurrentAnimatorStateInfo(0).nameHash == Animator.StringToHash("Base Layer.Heading"))
+        || animator.GetCurrentAnimatorStateInfo(0).nameHash == Animator.StringToHash("Base Layer.Heading"))
         {
             return;
         }
-
         else // 죽지않았을경우 (death == false)
         {
             if (animator)
@@ -123,37 +134,51 @@ public class PlayerControl : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        Move(); 
+
+        Move();
         Jump();
+
         //죽을경우
         if (health <= 0)
         {
             Death();
-
         }
         //Z키 누를시 박스생성 및 생성 지연시간 추가
-
         if (Input.GetKeyDown(KeyCode.Z))
         {
             health -= 2.0f;
             Invoke("CreateBoxFire", 1.2f); // 박스 생성지연 , 시간(1.0f초)
-            //GameObject BoxPosition = (GameObject)Instantiate (CreateBox); // Z키를 누르면 박스를 생성
-            //BoxPosition.transform.position = CreateBoxPosition.transform.position; 
-            //CreateBoxFire(); // 아래에 있음
+            this.audio_Create.clip = this.audio_Creating;
+            this.audio_Create.loop = false;
+            this.audio_Create.Play();
         }
 
         if (Input.GetKeyDown(KeyCode.X))
         {
-            Invoke("HeadingFire", 0.9f); // 헤딩 시 0.9f 뒤 HeadingFire 
+            Invoke("HeadingFire", 0.9f); // 헤딩 시 0.9f 뒤 HeadingFire
+            this.audio_Attack.clip = this.audio_Attacking;
+            this.audio_Attack.loop = false; 
+            this.audio_Attack.Play(); 
         }
 
-		if (Input.GetKeyDown(KeyCode.Escape))
-		{
-			Time.timeScale = 0;
-			Menu.enabled = true;
-		}
+//		if (Input.GetKeyDown(KeyCode.Escape))
+//		{
+//			Time.timeScale = 0;
+//			Menu.enabled = true;
+//		}
 
         UpdateHealthbar();
+
+        //치트키
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.Translate(new Vector3(40.0f, 20.0f, -0.6f));
+        }
+
+        if (Menu.enabled == true || SoundMenu.enabled == true) //메뉴진입시 못움직이게..
+        {
+            return;
+        }
     }
 
     //진입시
@@ -162,14 +187,18 @@ public class PlayerControl : MonoBehaviour {
         if (other.tag == "Enemy")
         {
             health -= 100.0f;
-            Death();           
+            Hit();
+            Death();
+            Invoke("Endure_Hit", 1);
         }
 
 
         if (other.tag == "Totem")
         {
             health -= 100.0f;
+            Hit();
             Death();
+            Invoke("Endure_Hit", 1);
         }
 
         UpdateHealthbar();
@@ -181,24 +210,28 @@ public class PlayerControl : MonoBehaviour {
         if (other.tag == "Axe")
         {
             health -= 0.2f;
+            Hit();
             Death();
+            Invoke("Endure_Hit",1);
         }
 
         
         if (other.tag == "BoggleBoggle")
         {
             health -= 0.15f;
+            Hit();
             Death();
+            Invoke("Endure_Hit", 1);
         }
         
 
         if (other.tag == "Hand")
         {
             health -= 0.1f;
+            Hit();
             Death();
+            Invoke("Endure_Hit", 1);
         }
-
-
 
         UpdateHealthbar();
     }
@@ -206,7 +239,7 @@ public class PlayerControl : MonoBehaviour {
     //헤딩 시 전진
     void HeadingFire()
     {
-        if (death == true)
+        if (death == true || hit == true)
         {
             return;
         }
@@ -224,7 +257,7 @@ public class PlayerControl : MonoBehaviour {
     }
     void CreateBoxFire()
     {
-        if (death == true)
+        if (death == true || hit == true)
         {
             return;
         }
@@ -252,7 +285,26 @@ public class PlayerControl : MonoBehaviour {
         }
     }
 
+    //히트함수
 
+    void Hit()
+    { 
+        hit = true;
+        animator.SetBool("Hits", true);
+        if (!facingright)
+        {
+            transform.Translate(new Vector3(5.0f, 0.0f, 0.0f) * Time.deltaTime * Speed);
+        }
+        else if (facingright)
+        {
+            transform.Translate(new Vector3(-5.0f, 0.0f, 0.0f) * Time.deltaTime * Speed);
+        }
+    }
+    void Endure_Hit()
+    {
+        hit = false;
+        animator.SetBool("Hits", false);
+    }
     //데스함수
     void Death()
     {
